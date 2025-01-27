cee128cbca916d6243e31d5e1fea02f6e9df5e3d
Marko Pavlovic <pavlovic265@gmail.com>
2025-01-27 20:01:01 +0100

    add command git add

---
+++ commands/add.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:                "add",
+	Aliases:            []string{"ad"},
+	Short:              "A simple CLI tool to show Git add",
+	DisableFlagParsing: true,
+}
+
+func Add() *cobra.Command {
+	addCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		exeArgs := append([]string{"add"}, args...)
+		exeCmd := exec.Command("git", exeArgs...)
+		exeCmd.Stdout = os.Stdout
+		exeCmd.Stderr = os.Stderr
+
+		if err := exeCmd.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing git add: %v\n", err)
+			os.Exit(1)
+		}
+
+		return nil
+	}
+
+	return addCmd
+}
