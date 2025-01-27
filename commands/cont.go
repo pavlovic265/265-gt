d7a7453cd8b372d3a4693191598d1493dce8fcd2
Marko Pavlovic <pavlovic265@gmail.com>
2025-01-27 20:54:03 +0100

    add cont for resolving rabase conflicts

---
+++ commands/cont.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var contCmd = &cobra.Command{
+	Use:                "cont",
+	Short:              "Used only finished resloving conflicnts",
+	DisableFlagParsing: true,
+}
+
+func cont() *cobra.Command {
+	contCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		exeArgs := append([]string{"rebase", "--continue"}, args...)
+		exeCmd := exec.Command("git", exeArgs...)
+		exeCmd.Stdout = os.Stdout
+		exeCmd.Stderr = os.Stderr
+
+		if err := exeCmd.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing git cont: %v\n", err)
+			os.Exit(1)
+		}
+
+		return nil
+	}
+
+	return contCmd
+}
