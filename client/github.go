227d0d9b780f8a30b68af3365457cd803abd9eb2
Marko Pavlovic <pavlovic265@gmail.com>
2025-03-08 20:11:43 +0100

    update

--- client/github.go
+++ client/github.go
@@ -80,7 +80,7 @@ func (svc *gitHubCli) CreatePullRequest(args []string) error {
 		return err
 	}
 
-	exeArgs := []string{"pr", "create", "--title", "title", "--body", "body", "--assignee", strings.TrimSpace(acc.User), "--base", *parent}
+	exeArgs := []string{"pr", "create", "--title", "title", "--body", "body", "--assignee", strings.TrimSpace(acc.User), "--base", *parent, "--head", *branch}
 	err = svc.exe.WithGh().WithArgs(exeArgs).Run()
 	if err != nil {
 		return err
