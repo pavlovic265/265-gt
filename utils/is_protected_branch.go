d65d1c11f3393c0eceb0a7c68f5aaa9c26838e05
Marko Pavlovic <pavlovic265@gmail.com>
2025-03-18 14:45:11 +0100

    add forogoten commands (#26)

---
+++ utils/is_protected_branch.go
@@ -0,0 +1,12 @@
+package utils
+
+import "github.com/pavlovic265/265-gt/config"
+
+func IsProtectedBranch(branch string) bool {
+	for _, pBranch := range config.Config.LocalConfig.Protected {
+		if branch == pBranch {
+			return true
+		}
+	}
+	return false
+}
