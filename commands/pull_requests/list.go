deead72113df36372e84f750f253d186e831b0b8
Marko Pavlovic <pavlovic265@gmail.com>
2025-10-09 09:04:43 +0200

    add yank pr url (#53)

--- commands/pull_requests/list.go
+++ commands/pull_requests/list.go
@@ -45,29 +45,46 @@ func (svc listCommand) selectPullRequest(
 	prs []client.PullRequest,
 ) error {
 	var strPrs []string
+	var urls []string
 	for _, pr := range prs {
 		strPrs = append(strPrs, fmt.Sprintf("%d:%s", pr.Number, pr.Title))
+		urls = append(urls, pr.URL)
 	}
+
+	var initialCursor int = 0
+	var currentURL string
+	if len(prs) > 0 {
+		currentURL = prs[initialCursor].URL
+	}
+
 	initialModel := components.ListModel{
 		AllChoices: strPrs,
 		Choices:    strPrs,
-		Cursor:     0,
+		Cursor:     initialCursor,
 		Query:      "",
+		YankURL:    currentURL,
+		URLs:       urls,
 	}
 
 	program := tea.NewProgram(initialModel)
 
 	if finalModel, err := program.Run(); err == nil {
-		if m, ok := finalModel.(components.ListModel); ok && m.Selected != "" {
-			splited := strings.Split(m.Selected, ":")
-			prNumber, err := strconv.Atoi(splited[0])
-			if err != nil {
-				return fmt.Errorf("faild to get pr number id")
+		if m, ok := finalModel.(components.ListModel); ok {
+			if m.Yanked {
+				fmt.Printf("✅ %s yanked to clipboard!", currentURL)
+				return nil
 			}
+			if m.Selected != "" {
+				splited := strings.Split(m.Selected, ":")
+				prNumber, err := strconv.Atoi(splited[0])
+				if err != nil {
+					return fmt.Errorf("faild to get pr number id")
+				}
 
-			for _, pr := range prs {
-				if prNumber == pr.Number {
-					exec.Command("open", pr.URL).Start()
+				for _, pr := range prs {
+					if prNumber == pr.Number {
+						exec.Command("open", pr.URL).Start()
+					}
 				}
 			}
 		}
